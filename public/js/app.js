30f894da612d10b7e50dd7d0ca7b0260b66cdad0
mikey1977 <mmi@umich.edu>
2015-10-03 19:56:53 -1000

    foundation grid added, jquery accesses files, but does not generate on html

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,25 @@
   $.ajax({
 
     //access json files
-    url : "../assets/api/my_boards.json",
+    url : "../api/my_boards.json",
     method: "GET",
     success : function(data) {
+      // console.log(data);
+      $('section').append('<div>');
+      for (var i = 0; i < data.data.children.length; i++) {
 
-      $('section').append()
+        console.log(data.data.children[i].data.thumbnail)
+        var pugPics = $('<div class="small-12 medium-6 large-6 columns>', {
+          html : data.data.children[i].data.title
+        });
+        var thumbs = '<img src="' + data.data.children[i].data.thumbnail + '">';
+
+        $(pugPics).append(thumbs);
+        $('section > div').append(pugPics);
+      }
+    },
+    error : function(err) {
+      console.log(err);
     }
   })
-})
+})();
