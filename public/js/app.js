b220e8a6357703c336bca132755ac0435d081d8c
mikey1977 <mmi@umich.edu>
2015-10-05 19:50:43 -1000

    added ajax to populate html, reformatting needed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,10 @@
 "use strict";
 
+
+// $('img').width(200);
+$('p').width(500);
+// $('img').height(relative);
+
 (function() {
   $.ajax({
 
@@ -8,17 +13,28 @@
     method: "GET",
     success : function(data) {
       // console.log(data);
-      $('section').append('<div>');
+      $('section').append('<ul>');
+      // $('<div>').append('<ul>')
       for (var i = 0; i < data.data.children.length; i++) {
 
-        console.log(data.data.children[i].data.thumbnail)
-        var pugPics = $('<div class="small-12 medium-6 large-6 columns>', {
+        console.log(data.data.children[i].data.thumbnail);
+
+        var pics = $('<li class="listData">', {
+        // var pugPics = $('<div class="small-12 medium-6 large-6 columns>', {
           html : data.data.children[i].data.title
         });
         var thumbs = '<img src="' + data.data.children[i].data.thumbnail + '">';
+        var divvy = '<div>';
+        var author = 'author: ' + data.data.children[i].data.author;
+        var stuff = 'comments: ' + data.data.children[i].data.num_comments + ' : ' + 'score: ' + data.data.children[i].data.score;
 
-        $(pugPics).append(thumbs);
-        $('section > div').append(pugPics);
+        $(pics).append(thumbs, divvy, author, divvy, stuff);
+        $('section > ul').append(
+          $(pics)
+
+          );
+        // $(pugPics).append(thumbs);
+        // $('section > div').append(pugPics);
       }
     },
     error : function(err) {
